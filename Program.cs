a9515b13e4386daecf19546ed3a8e0d82b250e2b
Eric Schles <ericschles@gmail.com>
2014-08-05 15:37:05 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnnParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://rss.cnn.com/rss/edition_world.rss");
+            XmlNodeList itemNodes = xmlDoc.SelectNodes("//rss/channel/item");
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                XmlNode titleNode = itemNode.SelectSingleNode("title");
+                XmlNode dateNode = itemNode.SelectSingleNode("pubDate");
+                if ((titleNode != null) && (dateNode != null))
+                    Console.WriteLine(dateNode.InnerText + ": " + titleNode.InnerText);
+            }
+            Console.ReadKey();
+        }
+    }
+}
